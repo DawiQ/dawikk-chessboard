c807a1c59db889212714f486579f5b68f3b96316
Dawid Wrzesinski <dawikk@MacBook-Pro-Dawid.local>
2025-08-25 10:14:50 +0200

    Fixed paths

--- src/components/Square.js
+++ src/components/Square.js
@@ -9,18 +9,18 @@ import { useBoardTheme } from '../themeContext';
 
 // *** PIECE IMAGES - Requires assets folder ***
 const PIECES = {
-  br: require('../assets/br.png'),
-  bp: require('../assets/bp.png'),
-  bn: require('../assets/bn.png'),
-  bb: require('../assets/bb.png'),
-  bq: require('../assets/bq.png'),
-  bk: require('../assets/bk.png'),
-  wr: require('../assets/wr.png'),
-  wp: require('../assets/wp.png'),
-  wn: require('../assets/wn.png'),
-  wb: require('../assets/wb.png'),
-  wq: require('../assets/wq.png'),
-  wk: require('../assets/wk.png'),
+  br: require('../../assets/br.png'),
+  bp: require('../../assets/bp.png'),
+  bn: require('../../assets/bn.png'),
+  bb: require('../../assets/bb.png'),
+  bq: require('../../assets/bq.png'),
+  bk: require('../../assets/bk.png'),
+  wr: require('../../assets/wr.png'),
+  wp: require('../../assets/wp.png'),
+  wn: require('../../assets/wn.png'),
+  wb: require('../../assets/wb.png'),
+  wq: require('../../assets/wq.png'),
+  wk: require('../../assets/wk.png'),
 };
 
 // *** ANIMATED HINT COMPONENT - Flex Version ***
